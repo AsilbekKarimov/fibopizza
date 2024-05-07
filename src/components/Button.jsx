e0947e53b41fa2789bdc18e8c77479d3bfd320ae
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-05-07 18:16:34 +0500

    ASD

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <button className='px-[24px] py-[10px] bg-[#F7D22D] rounded-lg text-white'>В корзину</button>
+    <button className='px-[24px] py-[10px] bg-[#F7D22D] rounded-lg text-white font-bold hover:bg-yellow-300 active:scale-90 transition duration-300'>В корзину</button>
   )
 }
 

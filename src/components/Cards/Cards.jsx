8c980d6cb5527a4d64b7cb0c6303130b09b558cd
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-05-07 17:52:38 +0500

    Cards added

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from './Card'
+
+const Cards = () => {
+  return (
+    <div className='w-[82%] mx-auto'>
+      <p className='text-[#F7D22D] text-[32px] font-extrabold mb-[26px]'>Паста</p>
+      <div className='flex flex-wrap justify-between'>
+      <Card img="src/assets/image 10 (2).png" new title="Komol chichqoq kakaxa boq" description="Lbdsk sdkuvbjbv evkjrvek veoirvojl eorkvn " price=" 290$"/>
+      <Card img="src/assets/image 10 (2).png" new title="Komol chichqoq kakaxa boq" description="Lbdsk sdkuvbjbv evkjrvek veoirvojl eorkvn " price=" 290$"/>
+      <Card img="src/assets/image 10 (2).png" new title="Komol chichqoq chochoq boq" description="Lbdsk sdkuvbjbv evkjrvek veoirvojl eorkvn " price=" 290$"/>
+      <Card img="src/assets/image 10 (2).png" new title="Komol chichqoq kakaxa boq" description="Lbdsk sdkuvbjbv evkjrvek veoirvojl eorkvn " price=" 290$"/>
+      <Card img="src/assets/image 10 (2).png" new title="Komol chichqoq kakaxa boq" description="Lbdsk sdkuvbjbv evkjrvek veoirvojl eorkvn " price=" 290$"/>
+      <Card img="src/assets/image 10 (2).png" new title="Komol chichqoq kakaxa boq" description="Lbdsk sdkuvbjbv evkjrvek veoirvojl eorkvn " price=" 290$"/>
+      <Card img="src/assets/image 10 (2).png" new title="Komol chichqoq kakaxa boq" description="Lbdsk sdkuvbjbv evkjrvek veoirvojl eorkvn " price=" 290$"/>
+      <Card img="src/assets/image 10 (2).png" new title="Komol chichqoq kakaxa boq" description="Lbdsk sdkuvbjbv evkjrvek veoirvojl eorkvn " price=" 290$"/>
+      </div>
+    </div>
+  )
+}
+
+export default Cards
---
+++ src/components/other/NewBadge.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewBadge = () => {
+  return (
+    <div className='absolute top-5 right-2 bg-gradient-to-br from-red-500 to-red-600 text-white px-[13px] py-[1px] rounded-3xl border border-red-600'>
+      NEW
+    </div>
+  )
+}
+
+export default NewBadge

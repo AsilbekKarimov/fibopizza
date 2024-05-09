55b0ea85c78ac64de28f53c39fce3aa074c8d2b2
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-05-09 17:47:30 +0500

    Card json fixed + lazy loading

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -8,9 +8,9 @@ const Card = (props) => {
       <div className='w-[256px] h-[464px] flex  justify-start flex-col mb-[49px]'>
         <div className='flex justify-center mb-[8px]'>
             <div className='relative'>
-                <img src={props.img} alt="" />
+                <img src={props.img} className='w-[253px] h-[253px]' alt="" />
                 {
-                    props.new
+                    props.new === true
                     ?
                     <div><NewBadge/></div>
                     :
@@ -18,11 +18,15 @@ const Card = (props) => {
                 }
             </div>    
         </div>
+        <div className='flex flex-col justify-between h-full'>
+        <div className='flex flex-col'>
         <p className='text-[#797979] font-extrabold text-[24px] mb-[14px]'>{props.title}</p>
         <p className='text-[#686466] text-[13px] mb-[12px] leading-[19px] w-[256px] flex flex-wrap'>{props.description}</p>
+        </div>
         <div className='flex justify-between items-center'>
             <p className='text-[#231F20] text-[22px] font-bold'>{props.price}</p>
             <Button/>
+        </div>
         </div>
       </div>
     </div>
